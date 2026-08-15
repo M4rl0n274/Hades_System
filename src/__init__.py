b957c05e1150efa72f738c077e16ef003bb0d272
M4rl0n274 <marlonquinteri274gmail.com>
2026-08-15 13:25:47 -0500

    implementando Login

--- src/__init__.py
+++ src/__init__.py
@@ -11,11 +11,13 @@ def create_app(config_name='default'):
     from src.controllers.productos_controller  import producto_bp
     from src.controllers.clientes_controller  import cliente_bp
     from src.controllers.home_controller      import home_bp
+    from src.controllers.auth_controller      import auth_bp
     #* registrar Ruta
     app.register_blueprint(factura_bp,   url_prefix='/facturas')
     app.register_blueprint(vendedores_bp,   url_prefix='/vendedores')
     app.register_blueprint(producto_bp,   url_prefix='/productos')
     app.register_blueprint(cliente_bp,   url_prefix='/clientes')
     app.register_blueprint(home_bp,      url_prefix='/')
+    app.register_blueprint(auth_bp,      url_prefix='/auth')
 
     return app
